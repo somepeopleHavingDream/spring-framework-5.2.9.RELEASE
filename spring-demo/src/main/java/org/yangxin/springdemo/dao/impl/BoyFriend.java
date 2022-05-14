09299aa0de37e42e8a5444612464c0051c82acce
yangxin <1393003255@qq.com>
2022-05-14 22:32:10 +0800

    feat: 夺取doGetBean之剩余逻辑的扫荡

---
+++ spring-demo/src/main/java/org/yangxin/springdemo/dao/impl/BoyFriend.java
@@ -0,0 +1,13 @@
+package org.yangxin.springdemo.dao.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author yangxin
+ * 2022/5/14 18:17
+ */
+public class BoyFriend {
+
+	@Autowired
+	private GirlFriend girlFriend;
+}
---
+++ spring-demo/src/main/java/org/yangxin/springdemo/dao/impl/GirlFriend.java
@@ -0,0 +1,13 @@
+package org.yangxin.springdemo.dao.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author yangxin
+ * 2022/5/14 18:17
+ */
+public class GirlFriend {
+
+	@Autowired
+	private BoyFriend boyFriend;
+}
