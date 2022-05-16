a3a3a79c29eabc09df957a6dc49d94a0beab23a6
yangxin <1393003255@qq.com>
2022-05-16 15:05:49 +0800

    feat: 20220516

--- spring-demo/src/main/java/org/yangxin/springdemo/dao/impl/BoyFriend.java
+++ spring-demo/src/main/java/org/yangxin/springdemo/dao/impl/BoyFriend.java
@@ -1,15 +1,15 @@
-package org.yangxin.springdemo.dao.impl;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-/**
- * @author yangxin
- * 2022/5/14 18:17
- */
-@Component
-public class BoyFriend {
-
-	@Autowired
-	private GirlFriend girlFriend;
-}
+//package org.yangxin.springdemo.dao.impl;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * @author yangxin
+// * 2022/5/14 18:17
+// */
+//@Component
+//public class BoyFriend {
+//
+//	@Autowired
+//	private GirlFriend girlFriend;
+//}
